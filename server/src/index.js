77f920fb2c33c21886a97f83c76ca4566e00e684
Sarvajeet <sarvajeetshahktn@gmail.com>
2025-12-30 23:19:57 +0530

    update cors syntax

--- server/src/index.js
+++ server/src/index.js
@@ -15,7 +15,9 @@ const app = express();
 // Middleware
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true
+  credentials: true,
+  methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
+  allowedHeaders: ['Authorization', 'Content-Type'],
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
