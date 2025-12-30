db899a034075f55d4582a5021bcf91b997b87001
Sarvajeet <sarvajeetshahktn@gmail.com>
2025-12-30 23:33:12 +0530

    remove email port number

--- server/src/utils/email.util.js
+++ server/src/utils/email.util.js
@@ -5,8 +5,6 @@ const getTransporter = () => {
   if (!transporter) {
     transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      secure: false,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
